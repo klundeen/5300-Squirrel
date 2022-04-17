d732bf6c0331dea5f91a13e7dced0162ea948b1b
vincent.marklynn <vincent.marklynn@gmail.com>
2022-04-16 23:28:11 -0700

    Added Makefile, main() in SqlParser.cpp; Currently only printing argv[1]

--- SqlParser.cpp
+++ SqlParser.cpp
@@ -3,3 +3,23 @@
  * @author Vincent Marklynn, Yu Zhong (William)
  * 
  */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "db_cxx.h"
+#include "SQLParser.h"
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Usage: cpsc5300: dbenvpath" << std::endl;
+    return 1;
+  }
+
+  std::string envHome = argv[1];
+
+  std::cout << envHome << std::endl;
+
+}
